c01ea2c3cd3c83c08342fac39fc165ce4b5f5ccd
lobo772 <59614268+lobo772@users.noreply.github.com>
2025-02-20 10:14:48 +0100

    fix

--- public/index.js
+++ public/index.js
@@ -10,7 +10,7 @@ const app = (0, express_1.default)();
 const PORT = process.env.PORT || 5000;
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
-const DATA_FILE = "src/data.json";
+const DATA_FILE = "./data.json"; //const DATA_FILE = "public/data.json";
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en http://localhost:${PORT}`);
@@ -29,63 +29,26 @@ app.post("/transactions", (req, res) => {
     const newTransaction = req.body;
     fs_1.default.readFile(DATA_FILE, (err, data) => {
         let transactions = [];
-        if (!err)
-            transactions = JSON.parse(data.toString() || "[]");
+        transactions = JSON.parse(data.toString() || "[]");
         transactions.push(newTransaction);
         fs_1.default.writeFile(DATA_FILE, JSON.stringify(transactions, null, 2), (err) => {
             if (err)
-                return res.status(500).json({ error: "Error saving data" });
-            res.json({ message: "Transaction saved" });
+                return res.status(500).json({ error: "Error al guardar datos" });
+            res.json({ message: "Datos guardados" });
         });
     });
 });
-
-// app.delete("/transactions/:id", async (req, res) => {
-//     try {
-//       const transactionId = req.params.id;
-//       let transactions = JSON.parse(data || "[]");
-
-//     // Buscar si la transacción existe
-//     const transactionExists = transactions.some(tx => tx.id === transactionId);
-//     if (!transactionExists) {
-//       return res.status(404).json({ error: "Transaction not found" });
-//     }
-
-//     // Filtrar eliminando la transacción con el ID especificado
-//     transactions = transactions.filter(tx => tx.id !== transactionId);
-
-//     // Guardar el nuevo array sin la transacción eliminada
-//     await fs.writeFile(DATA_FILE, JSON.stringify(transactions, null, 2));
-  
-//       res.json({ message: "Transaction deleted" });
-//     } catch (error) {
-//       res.status(500).json({ error: "Error deleting transaction" });
-//     }
-//   });
-
-app.delete("/transactions/:id", async (req, res) => {
-    try {
-      const transactionId = req.params.id; // Captura el ID de la URL
-  
-      // Leer transacciones desde el archivo
-      const data = await fs.readFile(DATA_FILE, "utf8");
-      let transactions = JSON.parse(data || "[]");
-  
-      // Buscar si la transacción existe
-      const transactionExists = transactions.some(tx => tx.id === transactionId);
-      if (!transactionExists) {
-        return res.status(404).json({ error: "Transaction not found" });
-      }
-  
-      // Filtrar eliminando la transacción con el ID especificado
-      transactions = transactions.filter(tx => tx.id !== transactionId);
-  
-      // Guardar el nuevo array sin la transacción eliminada
-      await fs.writeFile(DATA_FILE, JSON.stringify(transactions, null, 2));
-  
-      res.json({ message: "Transaction deleted" });
-    } catch (error) {
-      res.status(500).json({ error: "Error deleting transaction" });
-    }
-  });
+app.delete("/transactions/:id", (req, res) => {
+    const transactionId = req.params.id;
+    fs_1.default.readFile(DATA_FILE, (err, data) => {
+        let transactions = [];
+        transactions = JSON.parse(data.toString() || "[]");
+        let updateTransactions = transactions.filter(tx => tx.id !== transactionId);
+        fs_1.default.writeFile(DATA_FILE, JSON.stringify(updateTransactions, null, 2), (err) => {
+            if (err)
+                return res.status(500).json({ error: "Error al guardar datos" });
+            res.json({ message: "Datos guardados" });
+        });
+    });
+});
 //# sourceMappingURL=index.js.map
